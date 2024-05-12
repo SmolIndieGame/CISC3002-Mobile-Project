d66f6c1932d3da8531b0c144d03e703ade868b18
smolindiegame <smolindiegame@gmail.com>
2024-05-12 14:33:05 +0800

    copy audio handling

--- app/src/main/java/com/example/toedy_project/QuizFragment.java
+++ app/src/main/java/com/example/toedy_project/QuizFragment.java
@@ -1,73 +1,135 @@
 package com.example.toedy_project;
 
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 
 import java.io.File;
+import java.util.Locale;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link QuizFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class QuizFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    MediaPlayer mediaPlayer;
+    Button play;
+    Button pause;
+    Button stop;
+    TextView time;
+    SeekBar seek;
+    boolean isTouchingSeek;
+    boolean suppressSeekCompleteEvent;
+    Utils.LoopHandle seekUpdater;
 
     public QuizFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment QuizFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static QuizFragment newInstance(String param1, String param2) {
-        QuizFragment fragment = new QuizFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_quiz, container, false);
-        ImageView image = view.findViewById(R.id.image_cover);
+        View root = inflater.inflate(R.layout.fragment_quiz, container, false);
+
+        Context context = requireContext();
+        File hint = new File(context.getFilesDir(), "hint/0");
+        File sound = new File(context.getFilesDir(), "sound/0");
+
+        ImageView image = root.findViewById(R.id.quiz_image_hint);
         Glide
                 .with(this)
-                .load(new File(requireContext().getFilesDir(), "hint/0")).into(image);
-        return view;
+                .load(hint).into(image);
+
+        play = root.findViewById(R.id.quiz_button_start);
+        pause = root.findViewById(R.id.quiz_button_pause);
+        stop = root.findViewById(R.id.quiz_button_stop);
+        time = root.findViewById(R.id.quiz_text_sound);
+        seek = root.findViewById(R.id.quiz_seek_sound);
+
+        mediaPlayer = MediaPlayer.create(context, Uri.fromFile(sound));
+        mediaPlayer.setOnCompletionListener(player -> {
+            play.setEnabled(true);
+            pause.setEnabled(false);
+            stop.setEnabled(false);
+        });
+
+        play.setEnabled(true);
+        pause.setEnabled(false);
+        stop.setEnabled(false);
+        play.setOnClickListener(v -> {
+            mediaPlayer.start();
+            play.setEnabled(false);
+            pause.setEnabled(true);
+            stop.setEnabled(true);
+        });
+        pause.setOnClickListener(v -> {
+            mediaPlayer.pause();
+            play.setEnabled(true);
+            pause.setEnabled(false);
+            stop.setEnabled(true);
+        });
+        stop.setOnClickListener(v -> {
+            mediaPlayer.pause();
+            suppressSeekCompleteEvent = true;
+            mediaPlayer.seekTo(0);
+            play.setEnabled(true);
+            pause.setEnabled(false);
+            stop.setEnabled(false);
+        });
+
+        seek.setMax(mediaPlayer.getDuration());
+        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                isTouchingSeek = true;
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                isTouchingSeek = false;
+                requireActivity().runOnUiThread(() -> {
+                    if (!mediaPlayer.isPlaying()) {
+                        mediaPlayer.start();
+                        play.setEnabled(false);
+                        pause.setEnabled(true);
+                        stop.setEnabled(true);
+                    }
+                    mediaPlayer.seekTo(seekBar.getProgress());
+                });
+            }
+        });
+
+        return root;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        seekUpdater = new Utils.LoopHandle(() -> {
+            if (!isTouchingSeek)
+                seek.setProgress(mediaPlayer.getCurrentPosition(), false);
+            final int position = mediaPlayer.getCurrentPosition();
+            final int seconds = position / 1000 % 60;
+            final int minutes = position / 1000 / 60;
+            time.setText(String.format(Locale.ENGLISH, "%02d:%02d", minutes, seconds));
+        }, 100);
+    }
+
+    @Override
+    public void onPause() {
+        seekUpdater.dispose();
+        super.onPause();
     }
 }
